d3d70d1c0c9124e5d41ea439582e542e339831ab
Federico Builes <federico.builes@gmail.com>
2012-09-24 18:36:45 -0500

    Adding the HTML for the landing page.

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,47 @@
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require bootstrap
+
+$(function(){
+
+  $("#add-new-text-item").click(function(e){
+    e.preventDefault()
+    addNewStripeItem("text");
+  });
+
+  $("#add-new-embed-item").click(function(e){
+    e.preventDefault();
+    addNewStripeItem("embed");
+  });
+
+  $("#add-new-image-item").click(function(e){
+    e.preventDefault();
+    addNewStripeItem("image");
+  });
+});
+
+var addNewStripeItem = function(selector){
+  var elem = $("#" + selector + "-template").clone();
+  elem.attr("id", "");
+  var seed = Math.random().toString().slice(2, -1);
+  var html = elem.html().replace(/\{\{seed\}\}/g, seed);
+  $(".stripe-items").append(html);
+  addClickEventHandlers();
+}
+
+var addClickEventHandlers = function() {
+  $("a.remove-stripe-item").click(function(e){
+    e.preventDefault();
+    $(this).closest("div").remove();
+  });
+}
