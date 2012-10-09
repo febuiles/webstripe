2a71c27049423133c01d4730247cd93d15085ae8
Federico Builes <federico.builes@gmail.com>
2012-10-08 19:47:16 -0500

    Adding routes with padded zeroes for stripes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def path_to_stripe(stripe)
+    id = "0" * (6 - stripe.id.to_s.length) + stripe.id.to_s
+    "/#{id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ov::Application.routes.draw do
+  get '/:id' => 'stripes#show', :constraints => {:id => /\d+/}
+
   devise_for :users
   resources :stripes
   namespace :admin do
