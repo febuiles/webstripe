e8b8c4db54389e44dd80b75fed30a7e54ed3cd3b
jdestradap <jdestradap@gmail.com>
2013-03-25 22:05:28 -0500

    Update schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130311231547) do
+ActiveRecord::Schema.define(:version => 20130318021025) do
 
   create_table "stripe_items", :force => true do |t|
     t.string   "item_type"
