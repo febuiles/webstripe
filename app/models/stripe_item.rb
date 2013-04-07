ea0fc551b0241d8e746d83ed0bdcc51447583e35
jdestradap <jdestradap@gmail.com>
2013-04-07 10:41:03 -0500

    Clean embed code from others tags

--- app/models/stripe_item.rb
+++ app/models/stripe_item.rb
@@ -4,7 +4,7 @@ class StripeItem < ActiveRecord::Base
   attr_accessible :content, :item_type, :stripe_id, :image, :_destroy, :position
   attr_accessor :_destroy
 
-  after_validation :update_item_type
+  after_validation :update_stripe_item
 
   mount_uploader :image, ImageUploader
 
@@ -28,15 +28,25 @@ class StripeItem < ActiveRecord::Base
 
   private
 
-  def update_item_type
-    print content
-    print item_type
+  def update_stripe_item
     if (item_type == "text")
       doc = Nokogiri::HTML(content)
-      if (not(doc.xpath("//iframe").empty? and doc.xpath("//embed").empty?))
-        print "embed"
-        self.item_type = "embed"
-      end
+      update_item_type(doc)
+    end
+  end
+
+  def update_item_type(doc)
+    if (not(doc.xpath("//iframe").empty? and doc.xpath("//embed").empty?))
+      self.item_type = "embed"
+      update_embed_content(doc)
+    end
+ end
+
+  def update_embed_content(doc)
+    if(doc.xpath("//iframe"))
+      self.content = doc.xpath("//iframe").first.to_html
+    elsif(doc.xpath("//embed"))
+      self.content = doc.xpath("//embed").first.to_html
     end
   end
 end
