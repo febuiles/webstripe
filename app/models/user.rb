582df65aac91454f99152fffc07d979c8b8b7ad7
jdestradap <jdestradap@gmail.com>
2013-04-09 11:53:28 -0500

    Validate username presence/uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :stripes
-
+  validates :username, presence: true, uniqueness: true
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :username
 
   def admin?
