8ecdcef10f58d531f189605983ff35d67b772325
jdestradap <jdestradap@gmail.com>
2013-04-24 10:19:27 -0500

    Use timestamps in file names

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
   storage :fog
 
+  def filename
+    @name ||= "#{timestamp}-#{super}" if original_filename.present? and super.present?
+  end
+
+  def timestamp
+    var = :"@#{mounted_as}_timestamp"
+    model.instance_variable_get(var) or model.instance_variable_set(var, Time.now.to_i)
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
