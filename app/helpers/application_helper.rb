62f9a55f6c7f3407c22099b3e5717cda7043b454
Federico Builes <federico.builes@gmail.com>
2013-04-30 13:47:00 -0300

    Clean the user URLs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     id = "0" * (6 - stripe.id.to_s.length) + stripe.id.to_s
     "/#{id}"
   end
+
+  def clean_url(url)
+    if url =~ /http/
+      url
+    else
+      "http://#{url}"
+    end
+  end
 end
