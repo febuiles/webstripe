b34040e9ff2c90e93d2e548bd9d2165df838e38b
Federico Builes <federico.builes@gmail.com>
2014-03-15 16:18:45 -0500

    Create individual path for webstripe.js

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module Ov
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += ["admin.css", "admin.js", "landing.css", "landing.js", "static.css", "static.js"]
+    config.assets.precompile += ["admin.css", "admin.js", "landing.css", "landing.js", "static.css", "static.js", "webstripe.js"]
     config.action_mailer.default_url_options = { host: 'webstripe.co' }
   end
 end
